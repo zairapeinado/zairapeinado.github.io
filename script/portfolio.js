fb4be937e207115f3f17124f3b2535c034da2140
Alberto Peinado <djpeinado@gmail.com>
2020-08-02 22:12:39 +0200

    v1 current production version

---
+++ script/portfolio.js
@@ -0,0 +1,128 @@
+var path = window.location.href.substring(0, window.location.href.lastIndexOf('/') + 1);
+
+var portfolio = [];
+
+portfolio.push({
+	id:			1,
+	title:		'BAR DE COPAS “LOUNGE”',
+	text:		'Bar de copas diseñado en un local de 210m2. La principal característica es la forma circular de la sala, con la barra central enmarcada por las dos columnas y botellero suspendido. Además, está pensado para que la atmósfera cambie de tonalidad según el tipo de ambiente. Dispone de escenario para conciertos, camerinos y guardarropa. Al exterior, es muy austero con fachada de acero cortén.',
+	properties:	{
+					'Cliente':'Escuela de Artes',
+					'Categoría':'Sin construir'
+				},
+	images:		11
+});
+
+portfolio.push({
+	id:			2,
+	title:		'TIENDA GOURMET “GOURMET EXPERIENCE”',
+	text:		'La idea principal surge de la diferenciación de los dos espacios principales, el de zona de exposición del de la zona de degustación. El objeto primordial del proyecto y más característico de la tienda Gourmet, son los elementos de venta: vinos y quesos. La temática tratada para el diseño del local, ha sido la estación de otoño, para ello, se han ambientado las paredes y techo con hojas junto con tonos ocres, para dar el ambiente otoñal cuando la hoja se desprende del árbol. De esta forma, la temática se ha evidenciado en el falso techo, creándose un juego de hojas orgánicas que marcan el paso a la zona de ventas y a la zona de degustación, creando todavía un ambiente más acogedor.',
+	properties:	{
+					'Cliente':'Escuela de Artes',
+					'Categoría':'Sin construir'
+				},
+	images:		10
+});
+
+portfolio.push({
+	id:			3,
+	title:		'BAR DE COCKTAIL “SPRITZ”',
+	text:		'Spritz surge de crear un local para cocktail donde lo más característico fueran los paramentos, blancos y ondulantes. A ello le acompaña el suelo de teselas hexagonales colocado de forma especial, los tonos azul y la celosía de cierre. Prima en su funcionalidad y organización del espacio.',
+	properties:	{
+					'Cliente':'Escuela de Artes',
+					'Categoría':'Sin construir'
+				},
+	images:		9
+});
+
+portfolio.push({
+	id:			4,
+	title:		'SALÓN DE BELLEZA “BEAUTY BENESSERE”',
+	text:		'Beauty Benessere es un local de dos plantas donde la idea surge de dar énfasis a la zona de entrada, espacio de doble altura. De esta forma, el espacio es observado como un único espacio donde se encuentran la recepción, la escalera y la sala de salón de belleza.<br><br>El objeto primordial del proyecto y más característico es la escalera de caracol. La helicoidal de la escalera enfatiza la doble altura con la recepción, la zona de espera y ventas, que junto con las molduras de formas orgánicas le da un juego especial a todo el espacio interior.',
+	properties:	{
+					'Cliente':'Escuela de Artes',
+					'Categoría':'Sin construir'
+				},
+	images:		13
+});
+
+portfolio.push({
+	id:			5,
+	title:		'OFICINAS DE DISEÑO GRÁFICO',
+	text:		'Estas oficinas se conciben como un espacio subdividido mediante mamparas, donde el espacio se articula entre la recepción, zona de boxes, despachos, sala de juntas y zona de servicios. Cada una de ellas tiene un tratamiento especial en las mamparas mediante vinilos llamativos.',
+	properties:	{
+					'Cliente':'Escuela de Artes',
+					'Categoría':'Sin construir'
+				},
+	images:		new Array(6)
+});
+
+portfolio.push({
+	id:			6,
+	title:		'TIENDA DE ROPA “STARDUST”',
+	text:		'Tienda de ropa con revestimiento interior en bloques de PVC verde perforados con iluminación indirecta, y acabados pulidos, con brillo y reflectantes.',
+	properties:	{
+					'Cliente':'Escuela de Artes',
+					'Categoría':'Sin construir'
+				},
+	images:		3
+});
+
+portfolio.push({
+	id:			7,
+	title:		'CENTRO NEUROPSIQUIÁTRICO “NUESTRA SEÑORA DEL CARMEN”',
+	text:		'“Módulo de Jóvenes con Discapacidades Conductuales” surge de la necesidad de ampliar la prestación de servicios de asistencia y de carácter no lucrativo del Centro Neuropsiquiátrico Nuestra Señora del Carmen existente dirigido por las Hermanas Hospitalarias del Sagrado Corazón.<br><br>El edificio es exento, de forma rectangular y dimensiones 15,5 x 64,85 m. La cubierta es en su mayor parte en forma de diente de sierra, con pendientes variables –en algunos puntos muy acusadas- y algunas zonas cubierta plana. Estas pendientes variables diferencian desde el exterior las zonas destinadas a personal y las zonas destinadas a los internos y reflejan el grado de actividad mental en las diferentes estancias del interior del edificio. Pero lo más característico del proyecto, es el color rojo del zinc de toda la envolvente.<br><br>La circulación es lineal, a ambos lados de un pasillo central muy amplio, para facilitar la orientación de las personas que vivirán en el centro. Se ha prestado especial atención al objetivo prioritario del programa; distribuir y dimensionar de forma adecuada los dormitorios, con módulos dobles que se repiten (dos dormitorios individuales con un baño compartido y dos dormitorios dobles con aseo).',
+	properties:	{
+					'Redacción Proyecto':'Daniel Borruey y José Javier Gallardo',
+					'Dirección de Obra':'Zaira Peinado y José Javier Gallardo',
+					'Cliente':'Hermanas Hospitalarias del Sagrado Corazón de Jesús',
+					'Situación':'Garrapinillos (Zaragoza)',
+					'Fotografías':'Jesús Granada',
+					'Categoría':'Construido',
+				},
+	images:		4
+});
+
+portfolio.push({
+	id:			8,
+	title:		'REHABILITACIÓN DE VIVIENDA EN SAMPER DE CALANDA',
+	text:		'La reforma llevada a cabo en la vivienda unifamiliar fue la rehabilitación de la fachada con mortero de cal manteniendo los huecos existentes y creando los de la última planta. Donde además se realizó un nuevo forjado y una nueva cubierta de estructura metálica a un agua con cubrición de teja árabe.',
+	properties:	{
+					'Cliente':'Alejandro Abadía',
+					'Categoría':'Construido',
+					'Redacción Proyecto':'Daniel Borruey',
+					'Dirección de Obra':'Zaira Peinado',
+					'Situación':'Samper de Calanda (Teruel)'
+				},
+	images:		4
+});
+
+portfolio.push({
+	id:			9,
+	title:		'CURSOS E INFOGRAFÍAS PARA CONCURSOS',
+	text:		'En este portfolio se muestran algunas de las imágenes realizadas para los cursos de formación realizados con Rhinoceros y 3DS Max, ambos con el motor de renderizado con V-Ray. Las instituciones han sido:'
+					+'<br><br>&nbsp;&nbsp;- UNIVERSA INAEM: 3DS Max + V-ray 2015, 2016'
+					+'<br>&nbsp;&nbsp;- UNIVERSA INAEM: Rhinoceros + V-ray + Grasshopper 2012, 2013, 2014'
+					+'<br>&nbsp;&nbsp;- DGA, CPIFP Corona de Aragón: Rhinoceros + V-ray 2013, Rhinoceros + Impresión digital 2013,<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;3DS Max + V-ray + Photoshop + Impresion digital 2016'
+					+'<br>&nbsp;&nbsp;- UNIZAR, Grado en Estudios de Arquitectura: Rhinoceros 2011, 2012, 2013, 2014'
+					+'<br>&nbsp;&nbsp;- Enseñalia: Rhinoceros + V-ray 2011',
+	properties:	{
+					'Cliente':'UNIVERSA INAEM, DGA CPIFP Corona de Aragón',
+					'Categoría':'Construido'
+				},
+	images:		11
+});
+/*
+portfolio.push({
+	id:			10,
+	title:		'Recorrido virtual con Unreal Engine 4',
+	text:		'Prueba de carrusel de videos:'
+					+'<br>&nbsp;&nbsp;- Texto de prueba',
+	properties:	{
+					'Cliente':'Cliente de prueba',
+					'Categoría':'Prueba'
+				},
+	images:		0,
+	videos:		['188004889', '181301285', '180014121']
+});
+*/
