ef3310936fd4a2cb7a5cee6745c0fd0dde9e083b
Zaira Peinado <zairaj@gmail.com>
2020-08-09 18:47:49 +0200

    cambio de toda la pagina

--- script/portfolio.js
+++ script/portfolio.js
@@ -4,69 +4,70 @@ var portfolio = [];
 
 portfolio.push({
 	id:			1,
-	title:		'BAR DE COPAS “LOUNGE”',
-	text:		'Bar de copas diseñado en un local de 210m2. La principal característica es la forma circular de la sala, con la barra central enmarcada por las dos columnas y botellero suspendido. Además, está pensado para que la atmósfera cambie de tonalidad según el tipo de ambiente. Dispone de escenario para conciertos, camerinos y guardarropa. Al exterior, es muy austero con fachada de acero cortén.',
+	title:		'VIVIENDA RESIDENCIAL',
+	text:		'Diseño de interiores para vivienda residencial. Vivienda unifamiliar en Residencial MB.<br>Espacio de salón, comedor y cocina',
 	properties:	{
-					'Cliente':'Escuela de Artes',
-					'Categoría':'Sin construir'
+					'Cliente':'Zaira Peinado',
+					'Categoría':'En construcción'
 				},
-	images:		11
+	images:		3
 });
 
 portfolio.push({
 	id:			2,
-	title:		'TIENDA GOURMET “GOURMET EXPERIENCE”',
-	text:		'La idea principal surge de la diferenciación de los dos espacios principales, el de zona de exposición del de la zona de degustación. El objeto primordial del proyecto y más característico de la tienda Gourmet, son los elementos de venta: vinos y quesos. La temática tratada para el diseño del local, ha sido la estación de otoño, para ello, se han ambientado las paredes y techo con hojas junto con tonos ocres, para dar el ambiente otoñal cuando la hoja se desprende del árbol. De esta forma, la temática se ha evidenciado en el falso techo, creándose un juego de hojas orgánicas que marcan el paso a la zona de ventas y a la zona de degustación, creando todavía un ambiente más acogedor.',
+	title:		'OFICINAS',
+	text:		'Diseño de oficinas con muro vegetal.',
 	properties:	{
-					'Cliente':'Escuela de Artes',
-					'Categoría':'Sin construir'
+					'Cliente':'Office Group',
+					'Categoría':'Construido'
 				},
-	images:		10
+	images:		4
 });
 
 portfolio.push({
 	id:			3,
-	title:		'BAR DE COCKTAIL “SPRITZ”',
-	text:		'Spritz surge de crear un local para cocktail donde lo más característico fueran los paramentos, blancos y ondulantes. A ello le acompaña el suelo de teselas hexagonales colocado de forma especial, los tonos azul y la celosía de cierre. Prima en su funcionalidad y organización del espacio.',
+	title:		'JOYERÍA',
+	text:		'Modelado 3D de joyería para el Ciclo de Grado Superior de Joyería Artística.',
 	properties:	{
-					'Cliente':'Escuela de Artes',
-					'Categoría':'Sin construir'
+					'Cliente':'Escuela de Arte',
+					'Categoría':'Formación'
+				},
+	images:		3
+});
+
+portfolio.push({
+	id:			4,
+	title:		'BAR DE COPAS “LOUNGE”',
+	text:		'Bar de copas diseñado en un local de 210m2. La principal característica es la forma circular de la sala, con la barra central enmarcada por las dos columnas y botellero suspendido. Además, está pensado para que la atmósfera cambie de tonalidad según el tipo de ambiente. Dispone de escenario para conciertos, camerinos y guardarropa.',
+	properties:	{
+					'Cliente':'Escuela de Arte',
+					'Categoría':'Proyecto'
 				},
 	images:		9
 });
 
 portfolio.push({
-	id:			4,
-	title:		'SALÓN DE BELLEZA “BEAUTY BENESSERE”',
-	text:		'Beauty Benessere es un local de dos plantas donde la idea surge de dar énfasis a la zona de entrada, espacio de doble altura. De esta forma, el espacio es observado como un único espacio donde se encuentran la recepción, la escalera y la sala de salón de belleza.<br><br>El objeto primordial del proyecto y más característico es la escalera de caracol. La helicoidal de la escalera enfatiza la doble altura con la recepción, la zona de espera y ventas, que junto con las molduras de formas orgánicas le da un juego especial a todo el espacio interior.',
-	properties:	{
-					'Cliente':'Escuela de Artes',
-					'Categoría':'Sin construir'
-				},
-	images:		13
-});
-
-portfolio.push({
 	id:			5,
-	title:		'OFICINAS DE DISEÑO GRÁFICO',
-	text:		'Estas oficinas se conciben como un espacio subdividido mediante mamparas, donde el espacio se articula entre la recepción, zona de boxes, despachos, sala de juntas y zona de servicios. Cada una de ellas tiene un tratamiento especial en las mamparas mediante vinilos llamativos.',
+	title:		'TIENDA GOURMET “GOURMET EXPERIENCE”',
+	text:		'La idea principal surge de la diferenciación de los dos espacios principales, el de zona de exposición del de la zona de degustación. El objeto primordial del proyecto y más característico de la tienda Gourmet, son los elementos de venta: vinos y quesos. La temática tratada para el diseño del local, ha sido la estación de otoño, para ello, se han ambientado las paredes y techo con hojas junto con tonos ocres, para dar el ambiente otoñal cuando la hoja se desprende del árbol. De esta forma, la temática se ha evidenciado en el falso techo, creándose un juego de hojas orgánicas que marcan el paso a la zona de ventas y a la zona de degustación, creando todavía un ambiente más acogedor.',
 	properties:	{
-					'Cliente':'Escuela de Artes',
-					'Categoría':'Sin construir'
+					'Cliente':'Escuela de Arte',
+					'Categoría':'Proyecto'
 				},
-	images:		new Array(6)
+	images:		7
 });
 
 portfolio.push({
 	id:			6,
-	title:		'TIENDA DE ROPA “STARDUST”',
-	text:		'Tienda de ropa con revestimiento interior en bloques de PVC verde perforados con iluminación indirecta, y acabados pulidos, con brillo y reflectantes.',
+	title:		'BAR DE COCKTAIL “SPRITZ”',
+	text:		'Spritz surge de crear un local para cocktail donde lo más característico fueran los paramentos, blancos y ondulantes. A ello le acompaña el suelo de teselas hexagonales colocado de forma especial, los tonos azul y la celosía de cierre. Prima en su funcionalidad y organización del espacio.',
 	properties:	{
-					'Cliente':'Escuela de Artes',
-					'Categoría':'Sin construir'
+					'Cliente':'Escuela de Arte',
+					'Categoría':'Proyecto'
 				},
-	images:		3
+	images:		7
 });
+
 
 portfolio.push({
 	id:			7,
@@ -96,33 +97,15 @@ portfolio.push({
 				},
 	images:		4
 });
-
 portfolio.push({
 	id:			9,
-	title:		'CURSOS E INFOGRAFÍAS PARA CONCURSOS',
-	text:		'En este portfolio se muestran algunas de las imágenes realizadas para los cursos de formación realizados con Rhinoceros y 3DS Max, ambos con el motor de renderizado con V-Ray. Las instituciones han sido:'
-					+'<br><br>&nbsp;&nbsp;- UNIVERSA INAEM: 3DS Max + V-ray 2015, 2016'
-					+'<br>&nbsp;&nbsp;- UNIVERSA INAEM: Rhinoceros + V-ray + Grasshopper 2012, 2013, 2014'
-					+'<br>&nbsp;&nbsp;- DGA, CPIFP Corona de Aragón: Rhinoceros + V-ray 2013, Rhinoceros + Impresión digital 2013,<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;3DS Max + V-ray + Photoshop + Impresion digital 2016'
-					+'<br>&nbsp;&nbsp;- UNIZAR, Grado en Estudios de Arquitectura: Rhinoceros 2011, 2012, 2013, 2014'
-					+'<br>&nbsp;&nbsp;- Enseñalia: Rhinoceros + V-ray 2011',
-	properties:	{
-					'Cliente':'UNIVERSA INAEM, DGA CPIFP Corona de Aragón',
-					'Categoría':'Construido'
-				},
-	images:		11
-});
-/*
-portfolio.push({
-	id:			10,
 	title:		'Recorrido virtual con Unreal Engine 4',
-	text:		'Prueba de carrusel de videos:'
-					+'<br>&nbsp;&nbsp;- Texto de prueba',
+	text:		'Realidad virtual con Unreal Engine 4'
+					+'<br>&nbsp;&nbsp;Son una serie de 3 videos donde se realizan recorridos virtuales del interior de vivienda residencial como de recorrido de exterior.',
 	properties:	{
-					'Cliente':'Cliente de prueba',
-					'Categoría':'Prueba'
+					'Cliente':'Zaira Peinado',
+					'Categoría':'Realidad Virtual'
 				},
 	images:		0,
 	videos:		['188004889', '181301285', '180014121']
 });
-*/
